7b9e0b220d3480dd615de734a2a93d20878eea62
wgerg1 <59773912+wgerg1@users.noreply.github.com>
2020-02-01 12:31:41 -0600

    Add files via upload

--- spacemission.py
+++ spacemission.py
@@ -1,9 +1,12 @@
+from objects import *
+from gamemap import *
+
 WIDTH = 800
 HEIGHT = 600
 roommap = []
 roomheight = 7
 roomwidth = 10
-roomnumber = 31
+roomnumber = 33
 OBJECT_LIST = [images.floor, images.pillar, images.soil]
 TILE_SIZE = 30
 top_left_x = 100
@@ -17,6 +20,9 @@ def draw():
                 screen.blit(OBJECT_LIST[tiledata], (j * TILE_SIZE + top_left_x, i * TILE_SIZE - OBJECT_LIST[tiledata].get_height() + top_left_y))
 
 def generate_room():
+    global roommap, roomwidth, roomheight
+    roomwidth = GAME_MAP[roomnumber][2]
+    roomheight = GAME_MAP[roomnumber][1]
     temprow = []
     floor_material =0
     wall_material = 1
@@ -37,5 +43,16 @@ def generate_room():
     for i in range(roomwidth):
         temprow.append(wall_material)
     roommap.append(temprow)
+
+    #add in exits
+    if GAME_MAP[roomnumber][3]: #data position of top exit
+        exitpos = roomwidth // 2 #floor division
+        roommap[0][exitpos] = floor_material
+        roommap[0][exitpos+1] = floor_material
+    if GAME_MAP[roomnumber][4]: #right exit
+        exitpos = roomheight // 2
+        roommap[exitpos][roomwidth-1] = floor_material
+        roommap[exitpos+1][roomwidth-1] = floor_material
+        roommap[exitpos+1][roomwidth-1] = floor_material
     print(roommap)
 generate_room()

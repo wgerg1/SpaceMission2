dd6ec71ca3049cf5ec73a728bffe098581416bb0
wgerg1 <59773912+wgerg1@users.noreply.github.com>
2020-02-15 14:30:27 -0600

    Add files via upload

--- spacemission.py
+++ spacemission.py
@@ -1,17 +1,27 @@
 from objects import *
 from gamemap import *
 from scenery import *
+from player import *
+import time
 
 WIDTH = 800
 HEIGHT = 600
 roommap = []
 roomheight = 7
 roomwidth = 10
-roomnumber = 45
+roomnumber = 31
 OBJECT_LIST = objects
 TILE_SIZE = 30
 top_left_x = 100
 top_left_y = 150
+
+#PLAYER VARIABLES
+playerX = 5
+playerY = 2
+playerOffsetX = 0
+playerOffsetY = 0
+playerDirection = "right"
+playerFrame = 0
 
 def draw():
     if len(roommap) > 0:
@@ -19,6 +29,11 @@ def draw():
             for j in range(roomwidth):
                 tiledata = roommap[i][j]
                 screen.blit(OBJECT_LIST[tiledata][0], (j * TILE_SIZE + top_left_x, i * TILE_SIZE - OBJECT_LIST[tiledata][0].get_height() + top_left_y))
+            if playerY == i:
+                p_img = PLAYER[playerDirection][playerFrame]
+                p_left = top_left_x + playerX * TILE_SIZE
+                p_top = top_left_y + playerY * TILE_SIZE - p_img.get_height()
+                screen.blit(p_img, (p_left, p_top))
 
 def generate_room():
     global roommap, roomwidth, roomheight
@@ -78,4 +93,43 @@ def generate_room():
             roommap[scenitem[1]][scenitem[2]] = scenitem[0]
 
     print(roommap)
+
+def game_loop():
+    global playerDirection, playerX, playerY, playerOffsetX, playerOffsetY, playerFrame
+
+    if playerFrame > 0:
+        playerFrame += 1
+        time.sleep(0.05)
+        if playerFrame == 5:
+            playerFrame = 0
+            playerOffsetX = 0
+            playerOffsetY = 0
+    if playerFrame == 0:
+        if keyboard.right:
+            playerDirection = "right"
+            playerX +=1
+            playerFrame = 1
+        if keyboard.left:
+            playerDirection = "left"
+            playerX -=1
+            playerFrame = 1
+        if keyboard.down:
+            playerDirection = "down"
+            playerY +=1
+            playerFrame = 1
+        if keyboard.up:
+            playerDirection = "up"
+            playerY -=1
+            playerFrame = 1
+
+    if playerFrame > 0 and playerDirection == "right":
+        playerOffsetX = -1 + (0.25 * playerFrame)
+    if playerFrame > 0 and playerDirection == "left":
+        playerOffsetX = 1 + (0.25 * playerFrame)
+    if playerFrame > 0 and playerDirection == "up":
+        playerOffsetY = -1 + (0.25 * playerFrame)
+    if playerFrame > 0 and playerDirection == "down":
+        playerOffsetY = 1 + (0.25 * playerFrame)
+    #runs multiple times per second
 generate_room()
+clock.schedule_interval(game_loop, 0.03)
